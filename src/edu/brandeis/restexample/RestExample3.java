de5723da56b75ff91c4efca9f8d890964e85f535
Pito Salas <pitosalas@gmail.com>
2011-06-23 08:10:36 -0400

    small tweak

--- src/edu/brandeis/restexample/RestExample3.java
+++ src/edu/brandeis/restexample/RestExample3.java
@@ -1,13 +1,22 @@
 package edu.brandeis.restexample;
 
 import java.io.IOException;
+
+import org.w3c.dom.NodeList;
+
 import us.monoid.web.Resty;
+import us.monoid.web.XMLResource;
 
 public class RestExample3 {
 	public static void main(String[] args) throws IOException, Exception {
 		Resty r = new Resty();
 		String user1 = r.xml("http://vogueable.heroku.com/users/1.xml").toString();
 		System.out.println(user1);
+		
+		XMLResource usr1 = r.xml("http://vogueable.heroku.com/users/1.xml");
+		NodeList usr1s = usr1.get("/user/gender");
+		System.out.println(usr1s.item(0).getNodeValue());
+		
 	}
 
 }
